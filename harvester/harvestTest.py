b9a6a242dab07ce3ff7e34abfe7fa69e32e0c56e
Leandro Zamudio <leandro.zamudioleon@gmail.com>
2020-05-04 11:19:34 -0400

    Harvester testing

--- harvester/harvestTest.py
+++ harvester/harvestTest.py
@@ -2,7 +2,7 @@ import urllib2, json
 url = "https://services1.arcgis.com/LsoiDXzijohT7g97/arcgis/rest/services/COVID19ChileAcciones/FeatureServer/1/query?where=1%3D1&outFields=CuarentenaID,Nombre,Estado,Alcance,FInicio,FTermino,Cut_Com,Detalle,Shape__Area,Shape__Length&outSR=4326&f=pgeojson"
 response = urllib2.urlopen(url)
 data = json.loads(response.read())
-fixedata =json.dumps(data,sort_keys=True, indent=4)
-file = open("../output/Cuarentenas-Geo.geojson", "w")
+fixedata =json.dumps(data,sort_keys=True, indent=4,separators=(',', ':'))
+file = open("../output/Cuarentenas-Test.geojson", "w")
 file.write(fixedata)
 file.close()
